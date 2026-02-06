89d5be4fbb1cc8a6f9d25f208e35b6ec70e47dc9
Lipsa0704 <bhuyanlipsalin07@gmail.com>
2026-02-06 15:53:17 +0530

    commit changes

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -27,13 +27,22 @@ function getReply(message) {
 }
 
 const ChatWindow = () => {
+const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
     return (
         <>
-            <div className="w-screen h-screen bg-pink-200 ">
+            <div className="w-screen h-screen bg-pink-200 mt-0">
                 <h2 className="text-center font-bold text-4xl ">Chat Box</h2>
-                <div className="w-[50%] h-[60%] border-2 ml-100 mt-30">
-
+                <div className="w-[50%] h-[60%] border-2 ml-100 mt-30 rounded">
+                
                 </div>
+                 <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type a message..."
+          className="flex-1 border rounded px-3 py-2 ml-100 mt-2 "
+        />
+        <button className="rounded h-10 w-20 bg-blue-600 border-2 ml-5" >Send</button>
             </div>
         </>
     )
